d8d67a689fe5f9cbcae1883363ed232934d33aaf
NeyberMachado <neybermachado@gmail.com>
2025-08-30 16:23:42 -0400

    Add files via upload

---
+++ hola mundo.py
@@ -0,0 +1,6 @@
+print("¡hola mi nombre es neyber!")
+
+nombre="neyber"
+edad=20
+print("yo me llamo " + nombre + " y tengo  " + str(edad) + " años.")
+print(f"yo me llamo {nombre} y tengo ¨{edad} años.")
